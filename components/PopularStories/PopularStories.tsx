40f0f4d780eaf5d26c510428f36799509f7466e5
Viktoriia P. <pohrebniakvika@gmail.com>
2026-02-23 23:33:10 +0100

    add popular stories
    Made with a card stub instead TravellersStoriesItem

--- components/PopularStories/PopularStories.tsx
+++ components/PopularStories/PopularStories.tsx
@@ -1,6 +1,33 @@
-const PopularStories = () => {
-    return (
-    <h2>PopularStoriesSection</h2>);
-}
+import css from "./PopularStories.module.css";
+import { fetchStories } from "@/lib/api/api";
+import StoryCardStub from "./StoryCardStub";
 
-export default PopularStories;
+const ITEMS_IN_LAYOUT = 4;
+
+export default async function PopularStories() {
+  const data = await fetchStories(1, 10);
+
+ const stories = (data.stories ?? [])
+  .slice()
+  .sort((a, b) => (b.favoriteCount ?? 0) - (a.favoriteCount ?? 0))
+    .slice(0, ITEMS_IN_LAYOUT);
+  
+  return (
+    <section className={css.section}>
+      <h2 className={css.h2}>Популярні історії</h2>
+
+      <div className={css.list}>
+        {stories.map((story) => (
+          <div key={story._id} className={css.item}>
+            <StoryCardStub story={story} />
+          </div>
+        ))}
+      </div>
+      <div className={css.footer}>
+  <button type="button" className={css.moreBtn}>
+    Переглянути всі
+  </button>
+</div>
+    </section>
+  );
+}
