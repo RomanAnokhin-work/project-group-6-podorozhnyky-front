56af4447ea24c88e8b2e2353d04f41e0cc842df1
Anatoliy Podyryako <podyryakoanatoliy@gmail.com>
2026-02-24 20:00:54 +0200

    completed card for all screen

---
+++ lib/api/clientApi.ts
@@ -0,0 +1,17 @@
+import {User} from "../../types/user";
+import {nextServer} from "./api";
+
+interface GetUsersResponse {
+    page: number;
+    perPage: number;
+    totalUser: number;
+    totalPages: number;
+    users: User[];
+}
+
+
+export const getUsers = async ():Promise<GetUsersResponse> => {
+const {data} = await nextServer.get<GetUsersResponse>('/users');
+console.log(data.users)
+return data
+}
