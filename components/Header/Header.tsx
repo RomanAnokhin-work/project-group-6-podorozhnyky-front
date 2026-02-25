489de5048dcf037869abc5e8ba2c56bcaf2d47f9
StunMasteR <162857412+StunMasteR@users.noreply.github.com>
2026-02-25 04:33:19 +0200

    fix

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -46,8 +46,8 @@ export default function Header() {
       <header className={css.header}>
         <div className={`container ${css.container}`}>
           <Link href="/" className={css.logo_link} aria-label="Home">
-            <svg className={css.logo_icon} width="23" height="23">
-              <use href="/icons.svg#icon-Favicon" />
+            <svg className={css.logo_icon} width="24" height="24">
+              <use href="/icons.svg#icon-Company-Logo" />
             </svg>
             <p className={css.logo_text}>Подорожники</p>
           </Link>
