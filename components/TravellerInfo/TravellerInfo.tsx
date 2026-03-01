2c022345536fce2c07e5dadf965991dc896f8e2d
Anatoliy Podyryako <podyryakoanatoliy@gmail.com>
2026-03-01 12:55:27 +0200

    feat:Change structure and realized authPage {login/registration}

--- components/TravellerInfo/TravellerInfo.tsx
+++ components/TravellerInfo/TravellerInfo.tsx
@@ -18,26 +18,12 @@ export default function TravellerInfo({ user }: TravellerInfoResponse) {
         width="112"
         height="112"/>
 
-      {/* <Image src="https://ftp.goit.study/img/harmoniq/users/6881563901add19ee16fcff5.webp" 
-        alt="Дмитро Романенко"
-        className={styles.avatar} 
-        width="112"
-        height="112"/> */}
-
       <div className={styles.content}>
         <h2 className={styles.name}>{user.name}</h2>
-        {/* <h2 className={styles.name}>Дмитро Романенко</h2> */}
-        <p className={styles.description}>{user.description}</p>
-        {/* <p className={styles.description}>
-          Привіт! Я Дмитро. Люблю знаходити приховані перлини у кожній поїздці та ділитися ними. Світ повний дивовижних відкриттів!
-        </p> */}
-        
+        <p className={styles.description}>{user.description}</p>        
         <Link href={`/travellers/${user._id}`} className={styles.linkButton}>
           Переглянути профіль
         </Link>
-        {/* <Link href={`/travellers`} className={styles.linkButton}>
-          Переглянути профіль
-        </Link> */}
       </div>
     </div>
   );
