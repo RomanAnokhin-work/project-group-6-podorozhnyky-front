a64ac079f9dd91ca8460ac6506186413332a0956
Neo131088 <neo131088@gmail.com>
2026-03-04 00:59:52 +0100

    add edit Fron

--- app/api/api.ts
+++ app/api/api.ts
@@ -4,3 +4,12 @@ export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: true,
 });
+export interface SendResetEmailCredentials {
+  email: string;
+}
+
+export const sendResetEmail = async (
+  credentials: SendResetEmailCredentials,
+): Promise<void> => {
+  await api.post("/auth/send-reset-email", credentials);
+};
