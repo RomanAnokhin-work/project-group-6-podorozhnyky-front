682d2c3bcf63de62bf2b410fddeb22d5ac19e403
Roman <roman.anokhin.work@gmail.com>
2026-03-06 06:01:59 +0200

    Fix/MetadatChange

--- app/(main)/stories/[storyId]/page.tsx
+++ app/(main)/stories/[storyId]/page.tsx
@@ -2,26 +2,7 @@ import StoryDetails from "@/components/StoryDetails/StoryDetails";
 import { fetchStoryById } from "@/lib/api/clientApi";
 import { emit } from "node:process";
 
-export async function generateMetadata({
-  params,
-}: {
-  params: { storyId: string };
-}) {
-  const story = await fetchStoryById(params.storyId);
 
-  return {
-    icons: {
-      icon: "/favicon-1.svg",
-    },
-    title: story.title,
-    description: story.article,
-    openGraph: {
-      title: story.title,
-      description: story.article,
-      images: [story.img],
-    },
-  };
-}
 
 export default function StoryPage({
   params,
